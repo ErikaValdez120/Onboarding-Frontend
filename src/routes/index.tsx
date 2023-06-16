f12f0a34e542a78d997b5aaf0c5acc7db4575b3c
ErikaValdez120 <ervaldez@andreani.com>
2023-06-16 10:34:40 -0300

    primer comit

---
+++ src/routes/index.tsx
@@ -0,0 +1,27 @@
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import PrincipalSkeleton from "../skeletons/Principal";
+
+const Home = lazy(() => import("../pages/Home"));
+const User = lazy(() => import("../pages/User"));
+const Person = lazy(() => import("../pages/Person"));
+
+import ProtectedRoute from "./ProtectedRoute";
+import InteractionRoute from "./InteractionRoute";
+
+export default function AppRoutes() {
+  return (
+    <Suspense fallback={<PrincipalSkeleton />}>
+      <Routes>
+        <Route element={<InteractionRoute />}>
+          <Route element={<ProtectedRoute />}>
+            <Route element={<User />} path="user" />
+            <Route element={<Person />} path="person" />
+          </Route>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </Suspense>
+  );
+}
